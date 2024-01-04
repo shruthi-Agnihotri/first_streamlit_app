92465fdcabea20409a32320605f0ff8089cf922d
shruthi-Agnihotri <155644054+shruthi-Agnihotri@users.noreply.github.com>
2024-01-04 19:47:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 streamlit.dataframe(my_fruit_list)
 
 
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+
 
 
 
